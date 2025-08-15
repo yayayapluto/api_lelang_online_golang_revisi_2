94498e91c79b83b1ad07daa6af2ac51a6769338d
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-08-15 14:18:02 +0700

    chore: adding timestamp model

--- entities/timestamp.go
+++ entities/timestamp.go
@@ -1,1 +1,8 @@
 package entities
+
+import "time"
+
+type Timestamp struct {
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
